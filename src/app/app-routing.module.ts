12f9fa30ce32818f6a30c73c34182dca22fcca2f
Anna Bagrowska <anna_bagrowska@wp.pl>
2019-09-05 09:59:57 +0200

    create planet-details & planets-list component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PlanetsListComponent } from './planets-list/planets-list.component';
+import { PlanetDetailsComponent } from './planet-details/planet-details.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: PlanetsListComponent
+  },
+  {
+    path: 'planet/:id',
+    component: PlanetDetailsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PlanetDetailsComponent } from './planet-details/planet-details.component';
+import { PlanetsListComponent } from './planets-list/planets-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PlanetDetailsComponent,
+    PlanetsListComponent
   ],
   imports: [
     BrowserModule,
